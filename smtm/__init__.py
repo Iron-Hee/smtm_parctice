9b701b3ff7634340c35c241331a4353a04f36795
Iron-Hee <maks012199@naver.com>
2023-05-03 00:11:11 +0900

    strategy_bnh 클래스 smtm/__init__.py에 추가

--- smtm/__init__.py
+++ smtm/__init__.py
@@ -1,7 +1,9 @@
 from .date_converter import DateConverter
 from .simulation_data_provider import SimulationDataProvider
+from .strategy_bnh import StrategyBuyAndHold
 
 __all__ = [
-  "SimulationDataProvider"
+  "SimulationDataProvider",
+  "StrategyBuyAndHold",
 ]
 __version__ = "0.1.0"
